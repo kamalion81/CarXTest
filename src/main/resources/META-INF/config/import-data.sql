802c660b9d8e21fc57e6a4e95e129e5fab8f325d
kamalion <kamalion81@gmail.com>
2018-05-02 18:04:42 +0300

    Создан процесс

---
+++ src/main/resources/META-INF/config/import-data.sql
@@ -0,0 +1,10 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  Kamalion
+ * Created: May 2, 2018
+ */
+
